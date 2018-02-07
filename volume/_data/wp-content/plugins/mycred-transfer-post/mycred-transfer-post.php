02aa7b3263d50c7cdc03c96947d8ec5f45da8e1a
dasmedium <angel-antonio@live.com>
2018-02-06 22:17:01 -0500

    fixed mycred-transfer plugin header

--- volume/_data/wp-content/plugins/mycred-transfer-post/mycred-transfer-post.php
+++ volume/_data/wp-content/plugins/mycred-transfer-post/mycred-transfer-post.php
@@ -1,35 +1,39 @@
-/**
- * Change Post Ownership
- * Changes the post author to the user who purchased the post using
- * the myCRED Sell Content add-on.
- * @requires myCRED 1.6 or higher!
- * @version 1.0
- */
+<?php
+/*
+Plugin Name: Change Post Ownership
+Plugin URI: https://www.codex.mycred.me
+Description: Changes the post author to the user who purchased the post using the myCRED Sell Content add-on.
+Version: .1
+Author: Das Medium/Mycred
+Author URI: http://www.dasmedium.co
+*/
+
 add_filter( 'mycred_add_finished', 'mycred_pro_change_post_author_on_sale', 10, 3 );
 function mycred_pro_change_post_author_on_sale( $reply, $request, $mycred ) {
 
-	// Only applicable to post purchases (if approved)
-	if ( $request['ref'] != 'buy_content' || $reply === false ) return $reply;
+        // Only applicable to post purchases (if approved)
+        if ( $request['ref'] != 'buy_content' || $reply === false ) return $reply;
 
-	// Make sure this is not the author payout
-	if ( ! isset( $request['data']['seller'] ) ) return $reply;
+        // Make sure this is not the author payout
+        if ( ! isset( $request['data']['seller'] ) ) return $reply;
 
-	extract( $request );
+        extract( $request );
 
-	// Start by getting the post from the reference id
-	$post = get_post( absint( $ref_id ) );
+        // Start by getting the post from the reference id
+        $post = get_post( absint( $ref_id ) );
 
-	// Make sure the post still exist
-	if ( isset( $post->post_author ) && $post->post_author != $user_id ) {
+        // Make sure the post still exist
+        if ( isset( $post->post_author ) && $post->post_author != $user_id ) {
 
-		// Update
-		wp_insert_post( array(
-			'ID'          => $post->ID,
-			'post_author' => $user_id
-		) );
+                // Update
+                wp_insert_post( array(
+                        'ID'          => $post->ID,
+                        'post_author' => $user_id
+                ) );
 
-	}
+        }
 
-	return $reply;
+        return $reply;
 
 }
+
